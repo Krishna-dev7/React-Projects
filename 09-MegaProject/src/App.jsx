d781ab0d0cb718b3ae395bd7a3d94bd625c8d4bb
Krishna-dev7 <suprabha205@gmail.com>
2024-06-27 23:23:42 +0530

    added authentication service and configured redux store

--- 09-MegaProject/src/App.jsx
+++ 09-MegaProject/src/App.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { login, logout } from "./store/authSlice";
+import authService from "./appwrite/auth";
 import './App.css'
-import conf from "./conf/conf";
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
 
+  useEffect( async () => {
+    const currUser = await authService.getCurrentUser();
+    if (currUser) {
+      useDispatch(login(currUser));
+    } else {
+      useDispatch(logout());
+    }
+    setLoading(false);
+  }, [] )
 
-  return (
-    <h2> Welcome to Mega Project </h2>
-  )
+  return !loading ? <div className='w-full h-full bg-green-400'>
+    <div className='w-1/2 h-1/2 m-auto bg-white rounded'>
+      Nothing has to display
+    </div>
+  </div>  :  null
+
 }
 
 export default App
